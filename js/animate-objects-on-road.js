99d623d9cd7a6d1b85979b833a20d22c2011d353
Sebbbenator <eaa25ssg@students.eaaa.dk>
2025-11-06 08:15:33 +0100

    frankklam

--- js/animate-objects-on-road.js
+++ js/animate-objects-on-road.js
@@ -6,6 +6,8 @@ const getBlueCar = document.getElementById("blueCar");
 const getPoliceCar = document.getElementById("policeCar");
 const sun = document.querySelector(".sun");
 const scene = document.querySelector(".scene");
+const simonCar = document.getElementById("simon");
+const tungcar = document.getElementById("tungtung");
 
 // lydobjekter
 const soundRedCar = new Audio("sound/redcarsound.wav");
@@ -14,6 +16,9 @@ const soundPoliceSiren = new Audio("sound/policecarsound.wav");
 //soundPoliceSiren.src = "../sound/policecarsound.wav";
 const soundBlueCar = new Audio("sound/bluecarsound.wav");
 //soundBlueCar.src = "../sound/bluecarsound.wav";
+const soundSimonCar = new Audio("sound/simoncarsound.mp3");
+
+const soundTungCar = new Audio("sound/tungtungsound.mp3");
 
 // Funktioner til animationer og lyd
 if (getRedCar) {
@@ -40,5 +45,19 @@ if (getPoliceCar) {
 if (sun && scene) {
   sun.addEventListener("click", () => {
     scene.classList.toggle("night");
+  });
+}
+
+if (simonCar) {
+  simonCar.style.cursor = "pointer";
+  simonCar.addEventListener("click", () => {
+    soundSimonCar.play();
+  });
+}
+
+if (tungcar) {
+  tungcar.style.cursor = "pointer";
+  tungcar.addEventListener("click", () => {
+    soundTungCar.play();
   });
 }
